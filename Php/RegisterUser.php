a15fa05b3cbd6e2681c85fe89a8c0245c8e273c5
Setiuo <48561554+Setiuo@users.noreply.github.com>
2019-03-19 23:22:25 +0800

    Add files via upload
    111

--- Php/RegisterUser.php
+++ Php/RegisterUser.php
@@ -4,6 +4,17 @@
 
     if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']))
     {
+        $sql = "SELECT Name from oj_user where Name = '$_POST[username]'";  
+        $result = mysql_query($sql);  
+        $hava = mysql_num_rows($result);  
+
+        if($hava)
+        {
+            echo "<script>alert('注册失败：用户已经存在；即将刷新页面。');</script>";
+            echo "<script>parent.RefreshPage();</script>";
+            return;
+        }
+
         $sql = 'INSERT INTO oj_user(`name`, `uid`, `password`, `jurisdicton`, `signature`, `email`, `regtime`, `logtime`, `fight`, `skin`) values("'.AlterString($_POST['username']).'", 3, "'.$_POST['password'].'", 0, "", "'.$_POST['email'].'", "'.date("Y-m-d").'", "'.date("Y-m-d").'", 1000, "spacelab")';
         $result = mysql_query($sql);
 
@@ -15,11 +26,13 @@
         }
         else
         {
-            echo "<script>alert('注册失败。');</script>";
+            echo "<script>alert('注册失败。即将刷新页面。');</script>";
+            echo "<script>parent.RefreshPage();</script>";
         }
     }
     else
     {
-        echo "<script>alert('注册失败。');</script>";
+        echo "<script>alert('注册失败，输入信息读取失败。');</script>";
+        echo "<script>parent.RefreshPage();</script>";
     }
 ?>
